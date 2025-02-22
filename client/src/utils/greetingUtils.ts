de521f217064b6b21bebe64f6906b78c66107816
Daniel Lambert <danjlambert95@gmail.com>
2025-02-22 18:39:13 +0100

    Update greetingUtils.ts

--- client/src/utils/greetingUtils.ts
+++ client/src/utils/greetingUtils.ts
@@ -3,6 +3,8 @@ export const getGreeting = (): string => {
   
   if (currentHour < 12) {
     return "Good morning";
+  } else if (currentHour < 18) {
+    return "Good afternoon";
   } else {
     return "Good evening";
   }
